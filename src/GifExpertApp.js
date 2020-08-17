ad392952cc5528ab69f31e6f3d3382e1fec9cde8
Franco <franco.vitalematus@gmail.com>
2020-08-16 20:24:11 -0400

    Gif Expert App: Crear Lista de Categorias

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    // const categories = ['Dragon Ball, Bob Esponja, Mortal Kombat'];
+    const [categories, SetCategories] = useState(['Dragon Ball', 'Bob Esponja', 'Mortal Kombat']);
+
+    const handleAdd = () => {
+        // SetCategories( [ 'Los Caballeros del Zodiaco', ...categories] );
+        SetCategories(cats => [...cats, 'Los Caballeros del Zodiaco']);
+    }
     return (
-        <div>
+        <>
             <h2>Gif Expert App</h2>
             <hr />
-        </div>
+
+            <button onClick={handleAdd}>Agregar</button>
+
+            <ol>
+                {
+                    categories.map(category => {
+                        return <li key={category}> {category}</li>
+                    })
+                }
+            </ol>
+
+        </>
     )
 }
