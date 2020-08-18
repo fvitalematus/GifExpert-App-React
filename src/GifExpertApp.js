76d4ee263bdd33dbc229aa720002c06d9fb9f9d5
Franco <franco.vitalematus@gmail.com>
2020-08-17 23:54:38 -0400

    Gif Expert App: Fetch API - Obtener las imagenes deseadas

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
     // const categories = ['Dragon Ball, Bob Esponja, Mortal Kombat'];
-    const [categories, setCategories] = useState(['Dragon Ball', 'Bob Esponja', 'Mortal Kombat']);
+    const [categories, setCategories] = useState(['Dragon Ball']);
 
     // const handleAdd = () => {
     // setCategories( [ 'Los Caballeros del Zodiaco', ...categories] );
@@ -14,14 +15,17 @@ export const GifExpertApp = () => {
     return (
         <>
             <h2>Gif Expert App</h2>
-            <AddCategory setCategories={ setCategories } />
+            <AddCategory setCategories={setCategories} />
             <hr />
 
             <ol>
                 {
-                    categories.map(category => {
-                        return <li key={category}> {category}</li>
-                    })
+                    categories.map(category => (
+                        <GifGrid
+                            key={category}
+                            category={category}
+                        />
+                    ))
                 }
             </ol>
 
